c4d661f166ad027b83fdbce3a0d113b7e53fb72a
Marcin Szuppe <mps4dev@gmail.com>
2022-08-12 18:31:25 +0200

    Init

---
+++ src/main/java/com/example/shitcoins/ShitcoinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.shitcoins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShitcoinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShitcoinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shitcoins/ShitcoinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shitcoins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShitcoinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
